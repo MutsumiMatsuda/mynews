41a5418990a5cdc45646c1a8a938b96095ea7d73
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2022-04-27 16:10:34 +0900

    20220426

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,9 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      //if (\App::environment('production')) {
+      if ("production" == env('APP_ENV', 'local')) {
         \URL::forceScheme('https');
-      //}
+      }
     }
 
     /**
